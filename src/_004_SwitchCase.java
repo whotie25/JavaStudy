135a408c9b916ebc6e2b2a8c128fc36d3392fe05
Whotie <whotie506@gmail.com>
2025-09-02 13:19:39 +0900

    Conditional Statement: Switch-Case.

---
+++ src/_004_SwitchCase.java
@@ -0,0 +1,28 @@
+public class _004_SwitchCase {
+	public static void main(String[] args) {
+		for(int i = 1; i < 13; i++) {
+			switch(i) {
+			case 3:
+			case 4:
+			case 5:
+				System.out.println("Spring");
+				break;
+			case 6:
+			case 7:
+			case 8:
+				System.out.println("Summer");
+				break;
+			case 9:
+			case 10:
+			case 11:
+				System.out.println("Autumn");
+				break;
+			case 12:
+			case 1:
+			case 2:
+				System.out.println("Winter");
+				break;
+			}
+		}
+	}
+}
