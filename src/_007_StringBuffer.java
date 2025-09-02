76b5ce7f6e5211a931a9d20c085d162b9166e9ef
Whotie <whotie506@gmail.com>
2025-09-03 00:28:53 +0900

    String buffer. append, insert, delete, ...

---
+++ src/_007_StringBuffer.java
@@ -0,0 +1,20 @@
+public class _007_StringBuffer{
+	public static void main(String[] args) {
+		StringBuffer buffer = new StringBuffer(); //Mutable Type
+		buffer.append("Hello");
+		buffer.append(", ");
+		buffer.append("World!");
+		
+		String str = buffer.toString();
+		
+		System.out.println(buffer);
+		System.out.println(str + "\n");
+		
+		buffer = new StringBuffer("Dm7 - CM7");
+		buffer.insert(4, "- G7 ");
+		System.out.println(buffer);
+		
+		buffer.delete(0, 6);
+		System.out.println(buffer);
+	}
+}
