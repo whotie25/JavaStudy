b612f4db08c72eb78a54d0b2b62b10eae0ad5c52
Whotie <whotie506@gmail.com>
2025-09-02 12:46:41 +0900

    Arithmetic Operator.

---
+++ src/_003_ArithmeticOperator.java
@@ -0,0 +1,12 @@
+public class _003_ArithmeticOperator {
+	public static void main(String[] args) {
+		float sqrt_2 = 1.414f;
+		int num = 4;
+		
+		System.out.println(sqrt_2 + " + " + num + " = " + (sqrt_2+num));
+		System.out.println(sqrt_2 + " * " + num + " = " + (sqrt_2*num) + "\n");
+		
+		System.out.println("3 / " + num + " = " + (3/num));
+		System.out.println("3.0 / " + num + " = " + (3.0/num));
+	}
+}
