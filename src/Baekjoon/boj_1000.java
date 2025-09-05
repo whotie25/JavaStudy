8947b5447c16980786749365458bf116a157b4bd
Whotie <whotie506@gmail.com>
2025-09-05 12:19:52 +0900

    solution: BOJ #1000

---
+++ src/Baekjoon/boj_1000.java
@@ -0,0 +1,19 @@
+//A+B
+
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class boj_1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		
+//		System.out.print(a+b);
+		System.out.print(sc.nextInt() + sc.nextInt());
+		
+		sc.close();
+	}
+}
